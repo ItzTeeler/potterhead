10510fc8165511ac13dc251d6a2ba3d4bb793081
itzteeler <tylernguyen518@gmail.com>
2024-06-21 13:47:31 -0700

    Changes

--- src/app/Components/ModalComponent.tsx
+++ src/app/Components/ModalComponent.tsx
@@ -1,22 +1,40 @@
 import React from 'react'
 
-const ModalComponent = (props: { character: any, onClose:any }) => {
+const ModalComponent = (props: { character: any, onClose: any }) => {
     if (!props.character) return null;
-  return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 overflow-y-auto">
-      <div className="bg-white p-6 rounded-lg max-w-md w-full max-h-screen overflow-y-auto">
-        <button className="text-right mb-4" onClick={props.onClose}>Close</button>
-        <img src={props.character.image || "/qquestionmark.jpg"} alt={props.character.name} className="w-full h-auto mb-4 rounded-[10px]" />
-        <h2 className="text-2xl font-bold mb-4">{props.character.name}</h2>
-        <p>{props.character.alive === true ? "I'm ALIVE" : "I'm Dead..."}</p>
-        <p>{props.character.gender}</p>
-        <p>{props.character.house}</p>
-        <p>{props.character.hairColour}</p>
-        <p>{props.character.dateOfBirth}</p>
-        <p>{props.character.species}</p>
-      </div>
-    </div>
-  )
+    return (
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 overflow-y-auto">
+            <div className="relative bg-white p-6 rounded-lg max-w-lg w-full max-h-screen overflow-y-auto flex flex-col">
+                <button className="absolute top-2 right-2 text-black bg-white rounded-full p-1" onClick={props.onClose}>
+                    &times;
+                </button>
+                <div className="flex flex-col md:flex-row items-center">
+                    <img
+                        src={props.character.image || "/qquestionmark.jpg"}
+                        alt={props.character.name}
+                        className="w-full md:w-1/2 h-[200px] md:h-[300px] object-cover mb-4 md:mb-0 rounded-[10px] md:mr-4"
+                    />
+                    <div className="flex flex-col items-start">
+                        <h2 className="text-2xl font-bold mb-4">{props.character.name}</h2>
+                        <p>{props.character.alive === true ? "I'm ALIVE" : "I'm Dead..."}</p>
+                        <p>Gender: {props.character.gender}</p>
+                        <p>House: {props.character.house}</p>
+                        <p>Hair Color: {props.character.hairColour}</p>
+                        <p>Date of Birth: {props.character.dateOfBirth}</p>
+                        <p>Species: {props.character.species}</p>
+                        <div>
+                            <p>Wand</p>
+                            <ul className="list-disc ml-5">
+                                <li>Core: {props.character.wand?.core}</li>
+                                <li>Length: {props.character.wand?.length}</li>
+                                <li>Wood: {props.character.wand?.wood}</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default ModalComponent
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,7 @@ export default function Home() {
   const [selectedCharacter, setSelectedCharacter] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [searchValue, setSearchValue] = useState('');
+  const [selectedHouse, setSelectedHouse] = useState('');
 
   useEffect(() => {
     const getData = async () => {
@@ -41,16 +42,58 @@ export default function Home() {
     );
     setFilteredCharacters(filtered);
   };
+
+  const filterByHouse = (house: any) => {
+    setSelectedHouse(house);
+    if (house === '') {
+      setFilteredCharacters(characterData);
+    } else {
+      const filtered = characterData.filter((character: any) => character.house === house);
+      setFilteredCharacters(filtered);
+    }
+  };
   return (
     <>
       <NavBarComponent searchValue={searchValue} onSearchChange={handleSearchChange} />
-      <div className="px-10 pb-10 pt-20">
+      <div className="px-10 pb-10 pt-[150px]">
+        <div className="flex justify-center mb-4">
+          <button
+            className={`mr-2 py-1 px-3 rounded-md ${selectedHouse === '' ? 'bg-blue-500 text-white' : 'bg-gray-300'}`}
+            onClick={() => filterByHouse('')}
+          >
+            All
+          </button>
+          <button
+            className={`mr-2 py-1 px-3 rounded-md ${selectedHouse === 'Gryffindor' ? 'bg-red-500 text-white' : 'bg-gray-300'}`}
+            onClick={() => filterByHouse('Gryffindor')}
+          >
+            Gryffindor
+          </button>
+          <button
+            className={`mr-2 py-1 px-3 rounded-md ${selectedHouse === 'Slytherin' ? 'bg-green-500 text-white' : 'bg-gray-300'}`}
+            onClick={() => filterByHouse('Slytherin')}
+          >
+            Slytherin
+          </button>
+          <button
+            className={`mr-2 py-1 px-3 rounded-md ${selectedHouse === 'Ravenclaw' ? 'bg-blue-500 text-white' : 'bg-gray-300'}`}
+            onClick={() => filterByHouse('Ravenclaw')}
+          >
+            Ravenclaw
+          </button>
+          <button
+            className={`mr-2 py-1 px-3 rounded-md ${selectedHouse === 'Hufflepuff' ? 'bg-yellow-500 text-white' : 'bg-gray-300'}`}
+            onClick={() => filterByHouse('Hufflepuff')}
+          >
+            Hufflepuff
+          </button>
+        </div>
         <div className="grid grid-cols-5 gap-5 text-center">
           {filteredCharacters.length > 0 ? (
-            filteredCharacters.map((character: any, index: number) => (
+            filteredCharacters.map((character: any, index: any) => (
               <div
                 key={index}
-                className="bg-white m-5 pb-6 border border-gray-200 rounded cursor-pointer"
+                className="bg-white m-5 pb-6 border-2 border-black rounded cursor-pointer hover:ring-4 ring-white"
                 onClick={() => handleCharacterClick(character)}
               >
                 <img
@@ -60,7 +103,7 @@ export default function Home() {
                   width={150}
                   height={150}
                 />
-                <p>{character.name}</p>
+                <p className="bg-black text-white">{character.name}</p>
               </div>
             ))
           ) : (
